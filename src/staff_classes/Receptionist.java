48ab4a8a8a90ed6e2e12e3d32349c1b4196474b6
King George <georgeofosumensah@gmail.com>
2021-03-14 11:33:04 +0000

    I added the initial Receptionist class structure

---
+++ src/staff_classes/Receptionist.java
@@ -0,0 +1,20 @@
+package staff_classes;
+
+public class Receptionist extends Staff {
+	
+	Receptionist(String firstname, String lastname){
+		super(firstname, lastname);
+	}
+	
+	public void RegisterPatients() {
+		
+	}
+	
+	public void EditPatientsDetails() {
+		
+	}
+	
+	public void AssignPatientsToNurse() {
+		
+	}
+}
