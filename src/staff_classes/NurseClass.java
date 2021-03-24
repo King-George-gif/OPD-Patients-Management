d62131cff4dd1cb56e29c088a252327361865f15
King George <georgeofosumensah@gmail.com>
2021-03-24 10:21:09 +0000

    Clean the project of all unused code

--- src/staff_classes/NurseClass.java
+++ src/staff_classes/NurseClass.java
@@ -1,31 +1,11 @@
 package staff_classes;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class NurseClass extends Staff{
-	public Queue<String> patientsQueue;
+	
 	public NurseClass(String firstname, String lastname){
 		super(firstname, lastname);
-		this.patientsQueue = new LinkedList<String>();
-	}
-	
-	public void addVitalToPatientFile() {
 		
 	}
 	
-	public void EditPatientVitalFile() {
-		
-	}
 	
-	public void AssignPatientsToDoctor() {
-		
-	}
-	
-	public void deQueuePatientQueue()
-	{
-		if(!this.patientsQueue.isEmpty()) {
-			this.patientsQueue.remove();
-		}
-	}
 }
